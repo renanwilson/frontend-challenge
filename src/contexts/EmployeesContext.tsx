40e953626a924a8f991bdc2b0d80f3186d81ec94
renanwilson <renan.alves.ra16@gmail.com>
2023-03-27 14:52:36 -0300

    feat: create employees context

---
+++ src/contexts/EmployeesContext.tsx
@@ -0,0 +1,54 @@
+import { IEmployees } from "interfaces/IEmployees";
+import React, {
+  createContext,
+  Dispatch,
+  ReactElement,
+  SetStateAction,
+  useContext,
+  useEffect,
+  useState,
+} from "react";
+import { getEmployees } from "services/employees";
+
+export const INITIAL_STATE_EMPLOYEES: IEmployees[] = [
+  {
+    id: 0,
+    admission_date: "",
+    name: "",
+    office: "",
+    phone: 0,
+    photo: "",
+  },
+];
+
+type EmployeesContextProps = {
+  employees: IEmployees[];
+  setEmployees: Dispatch<SetStateAction<IEmployees[]>>;
+};
+type ListContextProviderProps = {
+  children: ReactElement;
+};
+const INITIAL_STATE: EmployeesContextProps = {
+  employees: INITIAL_STATE_EMPLOYEES,
+  setEmployees: () => [],
+};
+
+export const EmployeesContext =
+  createContext<EmployeesContextProps>(INITIAL_STATE);
+
+export const useListContext = () => useContext(EmployeesContext);
+
+export const EmployeesContextProvider = ({
+  children,
+}: ListContextProviderProps) => {
+  const [employees, setEmployees] = useState(INITIAL_STATE_EMPLOYEES);
+  useEffect(() => {
+    getEmployees(setEmployees);
+  }, [setEmployees]);
+
+  return (
+    <EmployeesContext.Provider value={{ employees, setEmployees }}>
+      {children}
+    </EmployeesContext.Provider>
+  );
+};
