424810f865316bc8e2839e277102a8b1b15b4acc
renanwilson <renan.alves.ra16@gmail.com>
2023-03-28 16:45:17 -0300

    feat: set a value to search

--- src/components/SearchEmployees/SearchEmployees.tsx
+++ src/components/SearchEmployees/SearchEmployees.tsx
@@ -1,5 +1,17 @@
+import { useSearchEmployeesContext } from "contexts/SearchEmployeesContext";
 import { Search } from "./styles";
-
+import React from "react";
 export function SearchEmployees() {
-  return <Search type="search" placeholder="Pesquisar" />;
+  const { setSearchEmployees } = useSearchEmployeesContext();
+
+  const handleSearch = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setSearchEmployees(e.target.value.toLowerCase());
+  };
+  return (
+    <Search
+      type="search"
+      placeholder="Pesquisar"
+      onChange={(e) => handleSearch(e)}
+    />
+  );
 }
