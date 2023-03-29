68ed5adc574bef741b1b54e9df6fc0e75a39328f
renanwilson <renan.alves.ra16@gmail.com>
2023-03-29 12:33:27 -0300

    hotfix: change filter employees

--- src/hooks/useFilteredEmployees.ts
+++ src/hooks/useFilteredEmployees.ts
@@ -5,12 +5,17 @@ export function useFilteredEmployees() {
   const { employees } = useEmployeesContext();
   const { searchEmployees } = useSearchEmployeesContext();
   const replaceSearch = searchEmployees.replace(/[^a-zA-Z0-9]/g, "");
+  const replaceString = (value: string) => value.replace(/[^a-zA-Z0-9]/g, "");
   const filteredEmployees =
     searchEmployees.length > 0
       ? employees.filter(
           (employee) =>
-            employee.name.toLowerCase().includes(searchEmployees) ||
-            employee.office.toLowerCase().includes(searchEmployees) ||
+            replaceString(employee.name.toLowerCase()).includes(
+              replaceSearch
+            ) ||
+            replaceString(employee.office.toLowerCase()).includes(
+              replaceSearch
+            ) ||
             employee.phone.includes(replaceSearch)
         )
       : [];
